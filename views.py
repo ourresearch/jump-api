db049efca6baf19a2cd36c7034530db3e801ee2b
Heather Piwowar <hpiwowar@gmail.com>
2019-10-27 20:29:30 -0700

    try https

--- views.py
+++ views.py
@@ -39,7 +39,7 @@ def base_endpoint():
 
 @app.route('/favicon.ico')
 def favicon():
-    return redirect(url_for("static", filename="img/favicon.ico"))
+    return redirect(url_for("static", filename="img/favicon.ico", _scheme='https'))
 
 def get_clean_package(http_request_args):
     package = http_request_args.get("package", "demo")
