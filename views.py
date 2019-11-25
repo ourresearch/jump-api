77948f0313bbb09c3ac733eafa996173f7fac483
Heather Piwowar <hpiwowar@gmail.com>
2019-11-24 16:30:34 -0800

    redo order

--- views.py
+++ views.py
@@ -395,12 +395,13 @@ def package_id_get(package_id):
 
     my_jwt = get_jwt()
 
-    RunAsyncToRequestResponse("package/{}?jwt={}".format(package_id, my_jwt)).start()
     for scenario in my_package.unique_saved_scenarios:
         RunAsyncToRequestResponse("scenario/{}?jwt={}".format(scenario.scenario_id, my_jwt)).start()
         RunAsyncToRequestResponse("scenario/{}/slider?jwt={}".format(scenario.scenario_id, my_jwt)).start()
         RunAsyncToRequestResponse("scenario/{}/table?jwt={}".format(scenario.scenario_id, my_jwt)).start()
         RunAsyncToRequestResponse("scenario/{}/apc?jwt={}".format(scenario.scenario_id, my_jwt)).start()
+
+    RunAsyncToRequestResponse("package/{}?jwt={}".format(package_id, my_jwt)).start()
 
     my_timing.log_timing("after kicking off cache requests")
 
