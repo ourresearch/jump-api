b74901b9e699497e3cac44dbfa02497754293c8d
Heather Piwowar <hpiwowar@gmail.com>
2019-12-07 11:02:20 -0800

    add warm the cache

--- import_accounts.py
+++ import_accounts.py
@@ -185,35 +185,6 @@ def create_accounts(filename):
 
             print u"created {}, package_id={}".format(new_account.username, new_account.packages[0].package_id)
 
-    def warm_the_cache():
-        import os
-        import sys
-        import logging
-        import requests
-        import time
-        sys.path.insert(0, '../jump-api')
-        mpl_logger = logging.getLogger("matplotlib")
-        mpl_logger.setLevel(logging.WARNING)
-        import views
-        from package import Package
-        from util import elapsed
-
-        packages = Package.query.all()
-        for package in packages:
-            print u"\nstart: {} {}".format(package.package_id, package)
-            start_time = time.time()
-            url = "https://cdn.unpaywalljournals.org/data/common/{}?secret={}".format(
-                package.package_id, os.getenv("JWT_SECRET_KEY"))
-            headers = {"Cache-Control": "public, max-age=31536000"}
-            r = requests.get(url, headers=headers)
-            print u"1st: {} {} {}".format(package.package_id, r.status_code, elapsed(start_time))
-
-            start_time = time.time()
-            r = requests.get(url, headers=headers)
-            print u"2nd: {} {} {}".format(package.package_id, r.status_code, elapsed(start_time))
-
-
-
 # python import_accounts.py ~/Downloads/new_accounts.csv
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run stuff.")
