dcf047985d27c1b329642b0361acea9563e27264
Richard Orr <richard@ourresearch.org>
2020-03-24 14:24:03 -0400

    add new login, registration, and user info endpoints

---
+++ permission.py
@@ -0,0 +1,23 @@
+from sqlalchemy import ForeignKey
+
+from app import db
+
+from sqlalchemy.orm import relationship
+
+
+class Permission(db.Model):
+    __tablename__ = 'jump_permission'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, unique=True)
+
+    def __repr__(self):
+        return u"<{} ({}) {}>".format(self.__class__.__name__, self.id, self.name)
+
+
+class UserInstitutionPermission(db.Model):
+    __tablename__ = 'jump_user_institution_permission'
+    user_id = db.Column(db.Integer, ForeignKey('jump_user.id'), primary_key=True)
+    institution_id = db.Column(db.Integer, ForeignKey('jump_institution.id'), primary_key=True)
+    permission_id = db.Column(db.Integer, ForeignKey('jump_permission.id'), primary_key=True)
+
+    permission = relationship(Permission)
---
+++ user.py
@@ -0,0 +1,50 @@
+import datetime
+
+import shortuuid
+from sqlalchemy.orm import relationship
+
+from app import db
+from permission import UserInstitutionPermission
+
+
+class User(db.Model):
+    __tablename__ = 'jump_user'
+    id = db.Column(db.Text, primary_key=True)
+    username = db.Column(db.Text)
+    display_name = db.Column(db.Text)
+    password_hash = db.Column(db.Text)
+    created = db.Column(db.DateTime)
+    is_demo_user = db.Column(db.Boolean)
+
+    permissions = relationship(UserInstitutionPermission)
+
+    def __init__(self, **kwargs):
+        self.id = shortuuid.uuid()[0:12]
+        self.created = datetime.datetime.utcnow().isoformat()
+        super(User, self).__init__(**kwargs)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.display_name,
+            'email': self.username,
+            'user_permissions': self.permission_dicts()
+        }
+
+    def permission_dicts(self):
+        dicts = {}
+        for permission in self.permissions:
+            if permission.institution_id not in dicts:
+                dicts[permission.institution_id] = {
+                    'institution_id': permission.institution_id,
+                    'user_id': self.id,
+                    'user_email': self.username,
+                    'permissions': [permission.permission.name]
+                }
+            else:
+                dicts[permission.institution_id]['permissions'].append(permission.permission.name)
+
+        return dicts.values()
+
+    def __repr__(self):
+        return u"<{} ({}) {}>".format(self.__class__.__name__, self.id, self.display_name)
