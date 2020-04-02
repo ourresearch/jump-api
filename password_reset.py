16efb78957a704e8b52de7e9e26d0299491eeb43
Richard Orr <richard@ourresearch.org>
2020-04-02 15:00:36 -0400

    add password reset endpoints

---
+++ password_reset.py
@@ -0,0 +1,21 @@
+import datetime
+
+import secrets
+
+from app import db
+
+
+class ResetRequest(db.Model):
+    __tablename__ = 'jump_password_reset_request'
+    token = db.Column(db.Text, primary_key=True)
+    user_id = db.Column(db.Text, db.ForeignKey("jump_user.id"))
+    requested = db.Column(db.DateTime)
+    expires = db.Column(db.DateTime)
+
+    user = db.relationship("User")
+
+    def __init__(self, **kwargs):
+        self.token = secrets.token_urlsafe(32)
+        self.requested = datetime.datetime.utcnow()
+        self.expires = datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+        super(ResetRequest, self).__init__(**kwargs)
