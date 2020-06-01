b162affef2ef0766f1d3ffaf384e32740a769968
Richard Orr <richard@ourresearch.org>
2020-06-01 14:17:58 -0400

    purge more caches

--- purge_cache.py
+++ purge_cache.py
@@ -2,8 +2,13 @@ import os
 
 import requests
 
+import package
+from app import db
+from institution import Institution
+from scenario import refresh_perpetual_access_from_db, refresh_cached_prices_from_db
 
-def purge_the_cache(package_id):
+
+def purge_common_package_data_cache(package_id):
     api_url = 'https://api.cloudflare.com/client/v4/zones/{}/purge_cache'.format(os.getenv('CLOUDFLARE_ZONE_ID'))
 
     cache_url = 'https://cdn.unpaywalljournals.org/live/data/common/{}?secret={}'.format(
@@ -16,3 +21,12 @@ def purge_the_cache(package_id):
         headers={'Authorization': 'Bearer {}'.format(os.getenv('CLOUDFLARE_CACHE_API_KEY'))},
         json={'files': [cache_url]}
     )
+
+
+def purge_all_caches(package_id):
+    purge_common_package_data_cache(package_id)
+    refresh_perpetual_access_from_db(package_id)
+
+    my_package = db.session.query(package.Package).filter(package.Package.package_id == package_id).scalar()
+    my_package.clear_package_counter_breakdown_cache()
+    refresh_cached_prices_from_db(my_package.package_id, my_package.publisher)
