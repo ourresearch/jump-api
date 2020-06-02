d3818b0fb98f958dc54d49182295f9c81f5f761c
Richard Orr <richard@ourresearch.org>
2020-06-02 16:04:00 -0400

    fix perpetual access normalization signature

--- test/test_perpetual_access.py
+++ test/test_perpetual_access.py
@@ -34,3 +34,17 @@ class TestPerpetualAccess(unittest.TestCase):
 
         assert_equals(normalize('a few days ago'), ParseWarning.bad_date)
         assert_equals(normalize(None), None)
+
+    def test_imports_perpetual_access(self):
+        rows, warnings = PerpetualAccessInput.normalize_rows('test/test_files/perpetual_access/perpetual_access.csv')
+
+        self.assertItemsEqual(
+            [
+                {'issn': '0012-4508', 'start_date': None, 'end_date': datetime.datetime(2020, 1, 1).isoformat()},
+                {'issn': '0013-4694', 'start_date': datetime.datetime(1900, 1, 1).isoformat(), 'end_date': None},
+                {'issn': '0749-5978', 'start_date': datetime.datetime(1955, 11, 05).isoformat(), 'end_date': datetime.datetime(2017, 10, 26).isoformat()},
+            ],
+            rows
+        )
+
+        self.assertItemsEqual([], warnings)
