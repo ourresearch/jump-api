79a6e4cb3f51abcd1cecde156fe99ed63ee8a51e
Heather Piwowar <hpiwowar@gmail.com>
2021-03-01 21:12:36 -0800

    no caching

--- consortium.py
+++ consortium.py
@@ -65,7 +65,8 @@ def get_consortium_ids():
 def consortium_get_computed_data(scenario_id):
     start_time = time()
 
-    if scenario_id in ["tGUVWRiN", "scenario-QC2kbHfUhj9W"]:
+    # if scenario_id in ["tGUVWRiN", "scenario-QC2kbHfUhj9W"]:
+    if False:
         import boto3
         s3_client = boto3.client("s3")
         print u"made s3_client in consortium_get_computed_data"
