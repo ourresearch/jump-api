7b924eae95071414e5e5dbc3d2481d2e7d5b549d
Heather Piwowar <hpiwowar@gmail.com>
2021-05-16 08:38:01 -0700

    handle exceptions during parse_uploads

--- parse_uploads.py
+++ parse_uploads.py
@@ -22,63 +22,70 @@ from journal_price import JournalPriceInput
 def parse_uploads():
 
     while True:
-        command = u"""select * from jump_raw_file_upload_object where to_delete_date is not null"""
-        with get_db_cursor() as cursor:
-            cursor.execute(command)
-            raw_file_upload_rows_to_delete = cursor.fetchall()
-        for row_to_delete in raw_file_upload_rows_to_delete:
-            file = row_to_delete["file"]
-            package_id = row_to_delete["package_id"]
-            if file == "price":
-                JournalPriceInput().delete(package_id)
-            elif file == "perpetual-access":
-                PerpetualAccessInput().delete(package_id)
-            else:
-                report_name = "jr1"
-                if "-" in file:
-                    report_name = file.split("-")[1]
-                CounterInput().delete(package_id, report_name=report_name)
-            # the delete will also delete the raw row which will take it off this queue
+        try:
+            command = u"""select * from jump_raw_file_upload_object where to_delete_date is not null"""
+            with get_db_cursor() as cursor:
+                cursor.execute(command)
+                raw_file_upload_rows_to_delete = cursor.fetchall()
+            for row_to_delete in raw_file_upload_rows_to_delete:
+                file = row_to_delete["file"]
+                package_id = row_to_delete["package_id"]
+                if file == "price":
+                    JournalPriceInput().delete(package_id)
+                elif file == "perpetual-access":
+                    PerpetualAccessInput().delete(package_id)
+                else:
+                    report_name = "jr1"
+                    if "-" in file:
+                        report_name = file.split("-")[1]
+                    CounterInput().delete(package_id, report_name=report_name)
+                # the delete will also delete the raw row which will take it off this queue
 
+        except Exception as e:
+            print "Error: exception {} during parse_uploads".format(e)
 
-        upload_preprocess_bucket = "unsub-file-uploads-preprocess"
-        upload_finished_bucket = "unsub-file-uploads"
-        preprocess_file_list = s3_client.list_objects(Bucket=upload_preprocess_bucket)
-        for preprocess_file in preprocess_file_list.get("Contents", []):
-            filename = preprocess_file["Key"]
-            filename_base = filename.split(".")[0]
-            try:
-                package_id, filetype = filename_base.split("_")
-            except ValueError:
-                # not a valid file, skip it
-                continue
+        try:
+            upload_preprocess_bucket = "unsub-file-uploads-preprocess"
+            upload_finished_bucket = "unsub-file-uploads"
+            preprocess_file_list = s3_client.list_objects(Bucket=upload_preprocess_bucket)
+            for preprocess_file in preprocess_file_list.get("Contents", []):
+                filename = preprocess_file["Key"]
+                filename_base = filename.split(".")[0]
+                try:
+                    package_id, filetype = filename_base.split("_")
+                except ValueError:
+                    # not a valid file, skip it
+                    continue
 
-            print u"loading {} {}".format(package_id, filetype)
-            size = preprocess_file["Size"]
-            age_seconds = (datetime.datetime.utcnow() - preprocess_file["LastModified"].replace(tzinfo=None)).total_seconds()
+                print u"loading {} {}".format(package_id, filetype)
+                size = preprocess_file["Size"]
+                age_seconds = (datetime.datetime.utcnow() - preprocess_file["LastModified"].replace(tzinfo=None)).total_seconds()
 
-            s3_clientobj = s3_client.get_object(Bucket="unsub-file-uploads-preprocess", Key=filename)
-            contents_string = s3_clientobj["Body"].read()
-            with open(filename, "wb") as temp_file:
-                temp_file.write(contents_string)
+                s3_clientobj = s3_client.get_object(Bucket="unsub-file-uploads-preprocess", Key=filename)
+                contents_string = s3_clientobj["Body"].read()
+                with open(filename, "wb") as temp_file:
+                    temp_file.write(contents_string)
 
-            loader = None
-            if filetype.startswith("counter"):
-                loader = CounterInput()
-            elif filetype.startswith("perpetual-access"):
-                loader = PerpetualAccessInput()
-            elif filetype.startswith("price"):
-                loader = JournalPriceInput()
+                loader = None
+                if filetype.startswith("counter"):
+                    loader = CounterInput()
+                elif filetype.startswith("perpetual-access"):
+                    loader = PerpetualAccessInput()
+                elif filetype.startswith("price"):
+                    loader = JournalPriceInput()
 
-            if loader:
-                load_result = loader.load(package_id, filename, commit=True)
+                if loader:
+                    load_result = loader.load(package_id, filename, commit=True)
 
-                print u"moving file {}".format(filename)
-                s3_resource = boto3.resource("s3")
-                copy_source = {"Bucket": upload_preprocess_bucket, "Key": filename}
-                s3_resource.meta.client.copy(copy_source, upload_finished_bucket, filename)
-                s3_resource.Object(upload_preprocess_bucket, filename).delete()
-                print "moved"
+                    print u"moving file {}".format(filename)
+                    s3_resource = boto3.resource("s3")
+                    copy_source = {"Bucket": upload_preprocess_bucket, "Key": filename}
+                    s3_resource.meta.client.copy(copy_source, upload_finished_bucket, filename)
+                    s3_resource.Object(upload_preprocess_bucket, filename).delete()
+                    print "moved"
+
+        except Exception as e:
+            print "Error: exception {} during parse_uploads".format(e)
 
         sleep( 2 * random.random())
         # print ".",
