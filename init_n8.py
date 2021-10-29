29c8fff523c4d5bc23a6c7b4df525d059091e48d
Scott Chamberlain <sckott@fastmail.com>
2021-10-29 14:01:37 -0700

    add if for ill requests for sister univs: only do in classic model

--- init_n8.py
+++ init_n8.py
@@ -433,36 +433,37 @@ if __name__ == "__main__":
         results_ill.append(ill)
         results.append(tmp_res)
 
-    # do ill calculations; add result back to results list
-    print("calculating ill requests for sister universities ...\n")
-    ill_sum_by_issnl = pd.concat(results_ill).groupby('issn_l').sum()
-    ill_sum_by_issnl.reset_index(inplace=True) # make issn_l a column
-    each_univ = dict.fromkeys(group_jusp_id_list, 0)
-    for index, row in ill_sum_by_issnl.iterrows():
-        issn = row['issn_l']
-        # print("issn: {}".format(issn))
-        univ_subscribing = []
-        for jusp_id in group_jusp_id_list:
-            journals = universities[jusp_id].saved_scenario_ownpta.journals
-            issns = [x.issn_l for x in journals]
-            if issn in issns:
-                univ_subscribing.append(journals[issns.index(issn)].subscribed)
-            else:
-                univ_subscribing.append(False)
-        num_univ_subscribing = sum(univ_subscribing)
-        # print("    univ's subscribing: {}".format(num_univ_subscribing))
-        if num_univ_subscribing > 0:
-            num_ill_requests_split_evenly = row['downloads_ill'] / num_univ_subscribing
-            univ_to_assign_to = list(compress(group_jusp_id_list, univ_subscribing))
-            # print("    univ's: {}".format((*univ_to_assign_to,)))
-            for univ in univ_to_assign_to:
-                each_univ[univ] += num_ill_requests_split_evenly
+    if not parsed_args.coreplus:
+        # do ill calculations; add result back to results list
+        print("calculating ill requests for sister universities ...\n")
+        ill_sum_by_issnl = pd.concat(results_ill).groupby('issn_l').sum()
+        ill_sum_by_issnl.reset_index(inplace=True) # make issn_l a column
+        each_univ = dict.fromkeys(group_jusp_id_list, 0)
+        for index, row in ill_sum_by_issnl.iterrows():
+            issn = row['issn_l']
+            # print("issn: {}".format(issn))
+            univ_subscribing = []
+            for jusp_id in group_jusp_id_list:
+                journals = universities[jusp_id].saved_scenario_ownpta.journals
+                issns = [x.issn_l for x in journals]
+                if issn in issns:
+                    univ_subscribing.append(journals[issns.index(issn)].subscribed)
+                else:
+                    univ_subscribing.append(False)
+            num_univ_subscribing = sum(univ_subscribing)
+            # print("    univ's subscribing: {}".format(num_univ_subscribing))
+            if num_univ_subscribing > 0:
+                num_ill_requests_split_evenly = row['downloads_ill'] / num_univ_subscribing
+                univ_to_assign_to = list(compress(group_jusp_id_list, univ_subscribing))
+                # print("    univ's: {}".format((*univ_to_assign_to,)))
+                for univ in univ_to_assign_to:
+                    each_univ[univ] += num_ill_requests_split_evenly
 
-    # combine ill requests for sister universities into output list
-    for uni in results:
-        uni_jusp_id = uni[0] # jusp is first element in the list
-        num_ill_requests_by_journal = round(each_univ[uni_jusp_id]) # round to 0 sig digits
-        uni.append(num_ill_requests_by_journal) # append to the results list for each univ
+        # combine ill requests for sister universities into output list
+        for uni in results:
+            uni_jusp_id = uni[0] # jusp is first element in the list
+            num_ill_requests_by_journal = round(each_univ[uni_jusp_id]) # round to 0 sig digits
+            uni.append(num_ill_requests_by_journal) # append to the results list for each univ
 
     for result_number in range(0, len(results[0])):
         print(";".join([str(results[column_number][result_number]) for column_number in range(0, len(results))]))
