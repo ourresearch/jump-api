73bd1b4dbeae06e1b730f6e8de5d43b5a43cf258
Scott Chamberlain <sckott@fastmail.com>
2022-01-31 08:45:28 -0800

    change email from Mafalda to Alice for push/pull functionality

--- views.py
+++ views.py
@@ -1162,7 +1162,7 @@ def subscriptions_notifications_done_editing_post(scenario_id):
     # by default send email to consortium test email. if JISC, send to Mafalda
     email_for_notification = "team+consortiumtest@ourresearch.org"
     if 'jisc' in scenario_id:
-        email_for_notification = "Mafalda.Marques@jisc.ac.uk"
+        email_for_notification = "alice.hughes@jisc.ac.uk"
     email = create_email(email_for_notification, u"New push/pull submission", "push_pull_done_editing", {"data": {
         "institution_name": institution_name
     }})
