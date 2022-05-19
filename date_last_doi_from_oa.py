a2637d39759739ee35e5bca5ebde0b4a00fde55f
Scott Chamberlain <sckott@fastmail.com>
2022-05-19 14:47:07 -0700

    changed date_last_doi_from_oa.py to use async requests to openalex api

--- date_last_doi_from_oa.py
+++ date_last_doi_from_oa.py
@@ -1,113 +1,109 @@
+import time
 import argparse
 import re
 from datetime import datetime
 from dateutil.parser import parse
 import requests
+import httpx
+import asyncio
 import csv
 from requests.exceptions import RequestException
 
 from app import db
 from app import get_db_cursor
+from psycopg2 import sql
+from psycopg2.extras import execute_values
 from openalex import OpenalexDBRaw
 from openalex_date_last_doi import OpenalexDateLastDOIFromOA
+
+def make_chunks(lst, n):
+	for i in range(0, len(lst), n):
+		yield lst[i:i + n]
 
 # class Empty(object):
 # 	pass
 # self = Empty()
-# self.__class__ = DateLastDOI
+# self.__class__ = DateLastDoiOA
 
 class DateLastDoiOA:
-	def __init__(self, since_update_date=None):
-		self.api_url = "https://api.openalex.org/works?filter=host_venue.id:{}&per_page=1&sort=publication_date:desc&mailto=scott@ourresearch.org"
+	def __init__(self):
+		self.api_url = "https://api.openalex.org/works?filter=host_venue.id:{}&per_page=1&sort=publication_date:desc&mailto=team@ourresearch.org"
 		self.load_openalex()
-		self.load_datelastdois()
-		self.all_date_last_dois(since_update_date)
+		self.all_date_last_dois()
 
 	def load_openalex(self):
 		self.openalex_data = OpenalexDBRaw.query.all()
 		for x in self.openalex_data:
 			x.id_oa = re.search('V.+', x.id)[0]
+			x.date_last_doi = None
 		print(f"{len(self.openalex_data)} openalex_journals records found")
 
-	def load_datelastdois(self):
-		self.openalex_date_last_doi = OpenalexDateLastDOIFromOA.query.all()
-		self.openalex_date_last_doi_issnls = [w.issn_l for w in self.openalex_date_last_doi]
-		print(f"{len(self.openalex_date_last_doi)} openalex_date_last_doi_from_oa records found")
+	def all_date_last_dois(self):
+		self.openalex_data_chunks = list(make_chunks(self.openalex_data, 30))
 
-	def all_date_last_dois(self, since_update_date=None):
-		if since_update_date:
-			since_update_date_dt = parse(since_update_date)
-			not_update = list(filter(lambda x: x.updated > since_update_date_dt, self.openalex_date_last_doi))
-			not_update_issns = [w.issn_l for w in not_update]
-			self.openalex_data = list(filter(lambda x: x.issn_l not in not_update_issns, self.openalex_data))
-			print(f"Since update date: {since_update_date} - limiting to {len(self.openalex_data)} records")
-		
-		for x in self.openalex_data:
-			self.date_last_doi(x)
-			if getattr(x, 'date_last_doi', None):
-				try:
-					self.write_to_database(x)
-				except Exception as e:
+		async def get_data(client, journal):
+			try:
+				r = await client.get(self.api_url.format(journal.id_oa))
+				if r.status_code == 404:
 					pass
+			except RequestException:
+				return None
 
-	def write_to_database(self, journal):
-		res = OpenalexDateLastDOIFromOA.query.get(journal.issn_l)
-		if not res:
-			res = OpenalexDateLastDOIFromOA(journal)
-			res.openalex_id = journal.id_oa
-			db.session.add(res)
-		else:
-			res.date_last_doi = journal.date_last_doi
-			res.openalex_id = journal.id_oa
-			res.updated = datetime.utcnow().isoformat()
+			if (
+				r.status_code == 200
+				and r.json().get("results")
+				and r.json()["results"][0]
+			):
+				if not r.json()["results"][0].get("publication_date"):
+					pass
+				else:
+					try:
+						published = r.json()["results"][0]["publication_date"]
+						self.set_last_doi_date(journal, published)
+					except:
+						pass
 
-		db.session.commit()
+		async def fetch_chunks(lst):
+			async with httpx.AsyncClient() as client:
+				tasks = []
+				for s in lst:
+					tasks.append(asyncio.ensure_future(get_data(client, s)))
 
-	def date_last_doi(self, journal):
-		try:
-			r = requests.get(self.api_url.format(journal.id_oa))
-			if r.status_code == 404:
-				print(f"OpenAlex 404 for {journal.id_oa}, logging it")
-				self.cache.log_404(journal.id_oa)
-		except RequestException:
-			print(f"OpenAlex request failed for {journal.id_oa} HTTP: ({r.status_code})")
-			return None
+				async_results = await asyncio.gather(*tasks)
+				return async_results
 
-		if (
-			r.status_code == 200
-			and r.json().get("results")
-			and r.json()["results"][0]
-		):
-			if not r.json()["results"][0].get("publication_date"):
-				print("issue with id {} (no 'publication_date' field found; logging).".format(journal.id_oa))
-				self.cache.log_no_published(journal.id_oa)
-			else:
-				try:
-					published = r.json()["results"][0]["publication_date"]
-					self.set_last_doi_date(journal, published)
-				except:
-					print(
-						"issue with OpenAlex ID {}".format(
-							journal.issn_l
-						)
-					)
+		for i, item in enumerate(self.openalex_data_chunks):
+			asyncio.run(fetch_chunks(item))
+			time.sleep(4)
+
+		# write to database in bulk
+		self.write_to_database_bulk(self.openalex_data_chunks)
+
+	def write_to_database_bulk(self, data):
+		flattened_data = [w for sublist in data for w in sublist]
+		cols = OpenalexDateLastDOIFromOA.get_insert_column_names()
+		input_values = [(datetime.utcnow().isoformat(), w.issn_l, w.id_oa, w.date_last_doi,) for w in flattened_data]
+
+		# delete all rows
+		with get_db_cursor() as cursor:
+			cursor.execute("truncate openalex_date_last_doi_from_oa")
+
+		# insert all rows
+		with get_db_cursor() as cursor:
+			qry = sql.SQL("INSERT INTO openalex_date_last_doi_from_oa ({}) VALUES %s").format(
+				sql.SQL(', ').join(map(sql.Identifier, cols)))
+			execute_values(cursor, qry, input_values, page_size = 1000)
 						
 	@staticmethod
 	def set_last_doi_date(journal, published):
 		status_as_of = datetime.strptime(published, "%Y-%m-%d")
 		journal.date_last_doi = status_as_of.strftime('%Y-%m-%d')
-		print(
-			"setting OpenAlex ID {} with date last doi of {}".format(
-				journal.id_oa, status_as_of
-			)
-		)
 
-# heroku local:run python date_last_doi_from_oa.py --update --since_update_date="2022-05-13 15:26:35.051186"
+# heroku local:run python date_last_doi_from_oa.py --update
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--update", help="Update date last DOI table", action="store_true", default=False)
-	parser.add_argument("--since_update_date", help="Only work on ISSNs not updated since the date", default=None)
 	parsed_args = parser.parse_args()
 
 	if parsed_args.update:
-		DateLastDoiOA(parsed_args.since_update_date)
+		DateLastDoiOA()
