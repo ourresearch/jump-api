eaa421af119a88e7c88bf99cacdabbe7a2ac8211
Scott Chamberlain <sckott@fastmail.com>
2022-06-22 06:36:37 -0700

    more oa_tables changes

--- oa_tables.py
+++ oa_tables.py
@@ -7,14 +7,12 @@ from dateutil.parser import parse
 import httpx
 import asyncio
 from statistics import mean
-from math import floor, ceil
-from tqdm import tqdm
 from collections import defaultdict
+from psycopg2 import sql
+from psycopg2.extras import execute_values
 
 from app import db
 from app import get_db_cursor
-from psycopg2 import sql
-from psycopg2.extras import execute_values
 from openalex import OpenalexDBRaw
 
 def distinct_issnls(table):
@@ -61,71 +59,71 @@ def fetch_data_and_split(table):
     #     issn_dict[issn] = [w for w in rows if w['issn_l'] == issn]
     # return issn_dict
 
-def delete_rows(table, data):
+def delete_rows(table, x):
     keys_to_extract = ["issn_l", "fresh_oa_status", "year_int"]
-    []
+    delete_data = [{key: w[key] for key in keys_to_extract} for w in x]
+    delete_tuples = [tuple(w.values()) for w in delete_data]
     with get_db_cursor() as cursor:
-        query_str = "delete from {} where issn_l=%s and fresh_oa_status=%s and year_int=%s"
+        query_str = "delete from {} where (issn_l, fresh_oa_status, year_int) IN (%s)"
         qry = sql.SQL(query_str).format(sql.Identifier(table))
-        cursor.execute(qry, (,))
-    print(f"deleted {len(data)} rows in {table}")
+        execute_values(cursor, qry, delete_tuples, page_size = 10000)
+    
+    print(f"deleted {len(delete_tuples)} rows in {table}")
+
+def insert_rows(table, x):
+    keys_to_extract = ["issn_l", "fresh_oa_status", "year_int"]
+    for dct in x:
+        dct['updated'] = datetime.utcnow()
+    insert_tuples = [tuple(w.values()) for w in x]
+    with get_db_cursor() as cursor:
+        qry = sql.SQL("insert into {} values %s").format(sql.Identifier(table))
+        execute_values(cursor, qry, insert_tuples, page_size = 10000)
+    
+    print(f"deleted {len(delete_tuples)} rows in {table}")
 
 def year_count(rws, year):
     return list(filter(lambda x: x['year_int'] == year, rws))[0]['count']
 
-def mean_of_two_years(value_target, year_1, year_2):
+def mean_of_two_years(year_1, year_2):
     val_mean = mean([year_1, year_2])
-    match value_target < val_mean:
-        case True:
-            val_mean_rounded = floor(val_mean)
-        case False:
-            val_mean_rounded = ceil(val_mean)
-    
-    return val_mean_rounded
+    return round(val_mean)
 
-def correct_2020(data):
+def correct_2020(lst):
     """
     returns only the data thats changed. if none changed, an empty 
     """
-    colors = list(set([w['fresh_oa_status'] for w in data]))
+    colors = list(set([w['fresh_oa_status'] for w in lst]))
     # color = colors[1]
     fixed_data = []
     for color in colors:
         subset_2020 = []
-        subset = list(filter(lambda x: x['fresh_oa_status'] == color, data))
+        subset = list(filter(lambda x: x['fresh_oa_status'] == color, lst))
         years = set([w['year_int'] for w in subset if w['year_int'] in range(2019, 2022)])
         if 2020 not in years:
             fixed_data.append(subset_2020)
             continue
         
         subset_2020 = list(filter(lambda x: x['year_int'] == 2020, subset))[0]
+        changed = False
         
         if set([2019, 2020, 2021]) == years:
-            subset_2020['count'] = mean_of_two_years(
-                subset_2020['count'], year_count(subset, 2019), year_count(subset, 2021))
+            subset_2020['count'] = mean_of_two_years(year_count(subset, 2019), year_count(subset, 2021))
+            changed = True
         elif set([2019, 2020]) == years:
-            subset_2020['count'] = mean_of_two_years(
-                subset_2020['count'], year_count(subset, 2019), year_count(subset, 2020))
+            subset_2020['count'] = year_count(subset, 2019)
+            changed = True
         elif set([2020, 2021]) == years:
-            subset_2020['count'] = mean_of_two_years(
-                subset_2020['count'], year_count(subset, 2020), year_count(subset, 2021))
+            subset_2020['count'] = year_count(subset, 2021)
+            changed = True
+        elif set([2020]) == years:
+            subset_2020['count'] = None
+            changed = True
 
-        fixed_data.append(subset_2020)
+        if changed:
+            fixed_data.append(subset_2020)
 
     fixed_dicts = [dict(w) for w in fixed_data if w]
     return fixed_dicts
-
-# def update_changes(table, rows):
-#     with get_db_cursor() as cursor:
-#         for row in rows:
-#             query_str = """
-#                 UPDATE {}
-#                 SET count = %(count)s, updated = sysdate
-#                 WHERE issn_l = %(issn_l)s and fresh_oa_status = %(fresh_oa_status)s and year_int = %(year_int)s
-#                 """
-#             qry = sql.SQL(query_str).format(sql.Identifier(table))
-#             print(cursor.mogrify(qry, row))
-#             cursor.execute(qry, row)
 
 def make_params(venue, oa_status, submitted):
     parts = [
@@ -316,27 +314,18 @@ if __name__ == "__main__":
         print("correcting 2020 year data in with_bronze tables")
         for table, qry in tables_with_bronze.items():
             print(f"table: {table}")
-            # issnls = distinct_issnls(table)
             data = fetch_data_and_split(table)
             to_update = []
-            pbar = tqdm(total = len(data) - 1)
             for issn, lst in data.items():
-                print(f"{issn}")
-                # res = query_one(issn)
                 out = correct_2020(lst)
                 if out:
                     to_update.extend(out)
-                pbar.update(1)
-                # print(f"{len(out)}")
-                # if out:
-                #     update_changes(table, out)
-            pbar.close()
 
             # delete all rows that have changed data
-            delete_rows(to_update)
+            delete_rows(table, to_update)
 
             # insert new data 
-            insert_rows()
+            insert_rows(table, to_update)
 
         # make tables_no_bronze tables
         print("making no_bronze tables")
